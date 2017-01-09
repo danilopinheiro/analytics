330d014e5614a2fd885aad9cb91eedd5ef074bbb
Danilo Pinheiro <dan.augusto@gmail.com>
2017-01-09 10:14:07 -0200

    Bora lá!

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php echo ("envia/n"); ?>
+
+<script>
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-41747144-2', 'none');
+
+ga('set', 'page', '/recomendador.html');
+
+ga('send','pageview',{'dimension3':'x-men','dimension4':'danilo.pinheiro','dimension5':'0'});
+ga('send','pageview',{'dimension3':'forrest gump','dimension4':'danilo.pinheiro','dimension5':'1'});
+ga('send','pageview',{'dimension3':'breaking bad','dimension4':'danilo.pinheiro','dimension5':'1'});
+ga('send','pageview',{'dimension3':'better call saul','dimension4':'danilo.pinheiro','dimension5':'1'});
+ga('send','pageview',{'dimension3':'the others','dimension4':'danilo.pinheiro','dimension5':'0'});
+
+ga('send','pageview',{'dimension3':'x-men','dimension4':'everton.gago','dimension5':'0'});
+ga('send','pageview',{'dimension3':'forrest gump','dimension4':'everton.gago','dimension5':'1'});
+ga('send','pageview',{'dimension3':'breaking bad','dimension4':'everton.gago','dimension5':'0'});
+ga('send','pageview',{'dimension3':'os trapalhoes','dimension4':'everton.gago','dimension5':'1'});
+ga('send','pageview',{'dimension3':'as aventuras de chico bento','dimension4':'everton.gago','dimension5':'0'});
+
+/*
+ga('set', 'dimension3', 'x-men'); //ItemId
+ga('set', 'dimension4', 'danilo.pinheiro'); //UserId
+ga('set', 'dimension5', '0'); //HighLighted
+ga('send', 'pageview', 'index'); 
+
+ga('set', 'dimension3', 'forrest gump'); //ItemId
+ga('set', 'dimension4', 'danilo.pinheiro'); //UserId
+ga('set', 'dimension5', '1'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'breaking bad'); //ItemId
+ga('set', 'dimension4', 'danilo.pinheiro'); //UserId
+ga('set', 'dimension5', '1'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'better call saul'); //ItemId
+ga('set', 'dimension4', 'danilo.pinheiro'); //UserId
+ga('set', 'dimension5', '1'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'the others'); //ItemId
+ga('set', 'dimension4', 'danilo.pinheiro'); //UserId
+ga('set', 'dimension5', '0'); //HighLighted
+ga('send', 'pageview', 'index'); 
+//user:danilo.pinheiro
+
+ga('set', 'dimension3', 'x-men'); //ItemId
+ga('set', 'dimension4', 'everton.gago'); //UserId
+ga('set', 'dimension5', '0'); //HighLighted
+ga('send', 'pageview', 'index'); 
+
+ga('set', 'dimension3', 'forrest gump'); //ItemId
+ga('set', 'dimension4', 'everton.gago'); //UserId
+ga('set', 'dimension5', '1'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'breaking bad'); //ItemId
+ga('set', 'dimension4', 'everton.gago'); //UserId
+ga('set', 'dimension5', '0'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'os trapalhoes'); //ItemId
+ga('set', 'dimension4', 'everton.gago'); //UserId
+ga('set', 'dimension5', '1'); //HighLighted
+ga('send', 'pageview', 'index');
+
+ga('set', 'dimension3', 'as aventuras de chico bento'); //ItemId
+ga('set', 'dimension4', 'everton.gago'); //UserId
+ga('set', 'dimension5', '0'); //HighLighted
+ga('send', 'pageview', 'index');
+
+//user:everton.gago 
+
+/*
+ga('set', 'dimension2', 'medium');
+ga('send', 'pageview', '/level_1/');
+
+ga('set', 'dimension2', 'hard');
+ga('send', 'pageview', '/level_1/');
+
+ga('set', 'dimension2', 'medium');
+ga('send', 'pageview', '/level_4/');
+
+ga('set', 'dimension2', 'hard');
+ga('send', 'pageview', '/level_4/');
+
+/*
+ga('send', 'event', 'category', 'action', {'dimension1': 'apenas um teste'});
+
+ga('set', 'dimension2', 'easy');
+ga('send', 'pageview', '/level_1/');
+
+ga('set', 'dimension2', 'medium');
+ga('send', 'pageview', '/level_2/');
+
+ga('set', 'dimension2', 'hard');
+ga('send', 'pageview', '/level_3/');
+
+ga('set', 'dimension2', 'easy');
+ga('send', 'pageview', '/level_4/');
+
+ga('set', 'dimension2', 'medium');
+ga('send', 'pageview', '/level_5/');
+
+ga('set', 'dimension2', 'medium');
+ga('send', 'pageview', '/level_6/');
+*/
+
+//ga('send', 'event', 'Videos', 'play', 'Fall Campaign');
+
+</script>
+
+<?php echo ("enviado!\n"); ?>
